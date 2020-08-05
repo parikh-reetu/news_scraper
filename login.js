41218545dbdcfc1244486cfbed44e57facd432a9
Reetu Parikh <reetu1017@gmail.com>
2020-08-05 14:34:39 -0400

    added register

--- login.js
+++ login.js
@@ -1,4 +1,3 @@
-// test
 // to include packages, we store modules in these variables
 var mysql = require('mysql');
 var express = require('express');
@@ -53,6 +52,21 @@ app.get('/', function(request, response) {
     // console.log(__dirname) prints the path of where the file is stored
 });
 
+app.get('/register.html', function(request, response) {
+    response.sendFile('/register.html', { root: __dirname })
+});
+
+app.get('/home', function(request, response) {
+    // check if logged in attribute for the client is true
+    if (request.session.loggedin) {
+        response.send('Welcome back, ' + request.session.username + '!');
+    }
+    else {
+        response.send('Please login to view this page');
+    }
+    response.end();
+});
+
 app.post('/auth', function(request, response) {
     // request.body.HTMLname to get the user input 
     var username = request.body.username;
@@ -87,17 +101,64 @@ app.post('/auth', function(request, response) {
     }
 });
 
-// you use '/home' because you were redirected there when the user sucessfully logged in 
-app.get('/home', function(request, response) {
-    // check if logged in attribute for the client is true
-    if (request.session.loggedin) {
-        response.send('Welcome back, ' + request.session.username + '!');
+
+app.post('/addUser', function(request, response) {
+    var email = request.body.email;
+    var username = request.body.username;
+    var password = request.body.password;
+
+    if (email && username && password) {
+        connection.query('SELECT * FROM accounts WHERE email = ? OR username= ?', [email, username], function(error, results, fields) {
+            // if any error while executing above query, throw error
+            if (error) throw error;
+            if (results.length > 0) {
+                var usedUsername = false;
+                var usedEmail = false;
+                // iterate for all the rows in result to check is username or email is already in the database
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].username == username) {
+                        usedUsername = true;
+                        break;
+                    }
+                }
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].email == email) {
+                        usedEmail = true;
+                        break;
+                    }
+                }
+                if (usedUsername && usedEmail) {
+                    response.send('A user has already registered with this username and email');
+                    response.end();
+                }
+                else if (usedUsername) {
+                    response.send('A user has already registered with this username');
+                    response.end();
+                }
+                else {
+                    response.send('A user has already registered with this email');
+                    response.end();
+                }
+            }
+            else {
+                connection.query('INSERT INTO accounts(username, password, email) VALUES(?, ?, ?)', [username, password, email], function() {
+                    if (error) throw error;
+                });
+                response.send('User has been registered');
+                response.end();
+            }
+
+        });
     }
     else {
-        response.send('Please login to view this page');
+        response.send('Please enter email, username, and password');
+        response.end();
     }
-    response.end();
+
 });
+
+// you use '/home' because you were redirected there when the user sucessfully logged in 
+
 
 // listening on a port creates a server
 app.listen(5555, function() {
